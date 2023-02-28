905b7eadd38edc43d4f1935fbf50276c1c871dd9
Abdur Rakib <abdurrakib961@gmail.com>
2023-02-28 16:30:06 +0600

    feat: setup redux basic configuration

---
+++ src/store/postSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  posts: [],
+  likedPosts: [],
+};
+
+export const postSlice = createSlice({
+  name: "post",
+  initialState,
+  reducers: {
+    setPosts: (state) => {},
+    likePost: (state) => {},
+    unlikePost: (state) => {},
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { setPosts, likePost, unlikePost } = postSlice.actions;
+
+export default postSlice.reducer;
